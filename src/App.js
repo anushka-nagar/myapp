d92adda99dbe6163fe308c9da88eebbb1eaaaa0f
Anushka Nagar <anushka.nagar@hotmail.com>
2021-06-22 17:38:56 +0530

    configuring reactstrap
    install bootsrap reactstrap react popper
    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Navbar , NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Restorante con fusion</NavbarBrand>
+        </div>
+      </Navbar>        
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
